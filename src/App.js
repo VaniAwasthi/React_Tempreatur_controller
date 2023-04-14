3a6c55e80a43d323b980c0a4afc7fffc2bddabd3
Vani Awasthi <awasthivani95@gmail.com>
2023-04-14 16:03:44 +0530

    adding new changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Tempreature_control from "./Components/Tempreatur_control.js"
 
 function App() {
   return (
     <div className="App">
-      
+      <Tempreature_control/>
     </div>
   );
 }
