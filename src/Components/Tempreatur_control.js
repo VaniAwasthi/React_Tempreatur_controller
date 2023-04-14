a02caee706165591fca0a5f45153c1cf535528a3
Vani Awasthi <awasthivani95@gmail.com>
2023-04-14 16:43:46 +0530

    last commit of project

--- src/Components/Tempreatur_control.js
+++ src/Components/Tempreatur_control.js
@@ -3,31 +3,34 @@ import "./Tempreature_control.css"
 
 function Tempreature_control(){
     const [temperatureValue, setTemperatureValue] = useState(10);
-  const [temperatureColor, setTemperatureColor] = useState("cold");
-
-  const increaseTemperature = () => {
-    if (temperatureValue === 30) return;
-
-    const newTemperature = temperatureValue + 1;
-
-    setTemperatureValue(newTemperature);
-    if (newTemperature >= 15) {
-      setTemperatureColor("hot");
-    }
-  };
-
-  const decreaseTemperature = () => {
-    if (temperatureValue === 0) return;
-
-    const newTemperature = temperatureValue - 1;
-    setTemperatureValue(newTemperature);
-    if (newTemperature < 20) {
-      setTemperatureColor("cold");
-    }
-  };
-
-    return(
-        <div className="app-container">
+    const [temperatureColor, setTemperatureColor] = useState("cold");
+    const [message, setMessage] = useState("Tempreature too cold");
+  
+    const increaseTemperature = () => {
+      if (temperatureValue === 30) return;
+  
+      const newTemperature = temperatureValue + 1;
+  
+      setTemperatureValue(newTemperature);
+      if (newTemperature >= 15) {
+        setTemperatureColor("hot");
+        setMessage("ooh tempreature is warm!");
+      }
+    };
+  
+    const decreaseTemperature = () => {
+      if (temperatureValue === 0) return;
+  
+      const newTemperature = temperatureValue - 1;
+      setTemperatureValue(newTemperature);
+      if (newTemperature < 20) {
+        setTemperatureColor("cold");
+        setMessage("ooh tempreature is cold !");
+      }
+    };
+  
+    return (
+      <div className="app-container">
         <div className="temperature-display-container">
           <div className={`temperature-display ${temperatureColor}`}>
             {temperatureValue}°C
@@ -37,7 +40,8 @@ function Tempreature_control(){
           <button onMouseDown={increaseTemperature}>+</button>
           <button onClick={decreaseTemperature}>-</button>
         </div>
+        <h2>{message}</h2>
       </div>
-    )
-}
+    );
+  };
 export default Tempreature_control
